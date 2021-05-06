116946ff4bce8b8db92c92318ce7d61c2b1d9f5e
aleja1201 <iraicyahh@gmail.com>
2021-05-06 09:30:31 -0500

    repositorio

--- src/HTMLscreens/baseMovil.js
+++ src/HTMLscreens/baseMovil.js
@@ -1,24 +1,6 @@
 export const templateMovil = () => {
 
     class baseMovil extends HTMLElement {
-<<<<<<< HEAD
-    constructor() {
-        super();
-
-    }
-    connectedCallback() {
-        this.innerHTML =  ` <section id="baseMovil">
-        <div class="container-barra">
-          <div class="logo">
-              <img src="./imagenes/cine (1).png" width='130vw'>
-          </div>
-          <div class="button-barra" id="publicar">
-            <a href="#publicar"><img src="./imagenes/galeria-de-imagenes.png"></a>
-          </div>
-          <div class="button-barra" id="perfil">
-            <a href="#perfil"><img src="./imagenes/usuario.png"></a>
-          </div>
-=======
         constructor() {
             super();
 
@@ -28,7 +10,6 @@ export const templateMovil = () => {
       <div class="container-barra">
         <div class="logo">
             <img src="./imagenes/cine (1).png" width='160vw'>
->>>>>>> b9f6d94c7243f174d180356d6104313dacfdd5ff
         </div>
         <div class="button-barra" id="publicar">
           <a href="#publicar"><img src="./imagenes/galeria-de-imagenes.png"></a>
