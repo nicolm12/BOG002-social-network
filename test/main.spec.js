acffb5a7aae4c22fd0ed255bdcf79d194df8e35c
carfolacam98 <laura.carfolacam@gmail.com>
2021-05-24 22:10:36 -0500

    pruebas test

--- test/main.spec.js
+++ test/main.spec.js
@@ -2,11 +2,19 @@
  import {init}   from '../src/main.js';
 
  
- describe('Init', () => {
-   console.log(init)
-   console.log(typeof init)
-   
-   it.skip('debería ser una función de inicializar', () => {
-    console.log("yo que se")
+ describe('Init', () => { 
+   it('debería ser una función de inicializar', () => {
+    expect(typeof init).toBe("function");
  });
  });
+
+ it('mock hash', () => {
+  const { location } = window;
+  delete window.location;
+  window.location = { reload: jest.fn() };
+
+  expect(window.location.reload).not.toHaveBeenCalled();
+  window.location.reload();
+  expect(window.location.reload).toHaveBeenCalled();
+  window.location = location;
+});
